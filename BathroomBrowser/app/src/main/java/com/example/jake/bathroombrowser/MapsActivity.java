a364c3ce2fbb01d05d412487069f02e920df7a8f
jakewinswoo <jslink@wpi.edu>
2017-02-27 19:51:35 -0500

    changes to mapactivity
    attempted to add support for current location, can't test on emulator
    permissions update in manifest.xml

--- BathroomBrowser/app/src/main/java/com/example/jake/bathroombrowser/MapsActivity.java
+++ BathroomBrowser/app/src/main/java/com/example/jake/bathroombrowser/MapsActivity.java
@@ -1,11 +1,18 @@
 package com.example.jake.bathroombrowser;
 
 import android.content.Intent;
+import android.location.Location;
+import android.provider.SyncStateContract;
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -13,9 +20,17 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.io.IOException;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private GoogleMap mMap;
+    public GoogleApiClient mGoogleApiClient = null;
+    public Location mLastLocation;
+    protected String mLatitudeLabel;
+    protected String mLongitudeLabel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,26 +40,61 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        // Create an instance of GoogleAPIClient.
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
     }
 
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        try{
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        }catch(SecurityException ex){
+            System.out.println("Permission error");
+        }
+        if (mLastLocation != null) {
+            mLatitudeLabel = (String.valueOf(mLastLocation.getLatitude()));
+            mLongitudeLabel =(String.valueOf(mLastLocation.getLongitude()));
+        }
+        centerMapOnMyLocation();
+    }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         // Add a marker in worcester and move the camera
         LatLng sydney = new LatLng(42.2626, -71.8023);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.addMarker(new MarkerOptions().position(sydney).title("You are here."));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+    }
+
+    private void centerMapOnMyLocation() {
+
+        LatLng myLocation = new LatLng(42.274752, -71.808331);
+        //LatLng myLocation = new LatLng(0, 0);
+
+        if(mLatitudeLabel != null && mLongitudeLabel != null){
+            System.out.println("we have labels");
+            System.out.println(mLatitudeLabel);
+            System.out.println(mLongitudeLabel);
+
+            myLocation = new LatLng(Integer.parseInt(mLatitudeLabel), Integer.parseInt(mLongitudeLabel));
+        }
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(myLocation,18));
     }
 
     public void listButtonOnClick(View v){
@@ -55,5 +105,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void settingsButtonOnClick(View v){
         Button button = (Button) v;
         startActivity(new Intent(getApplicationContext(),SettingsActivity.class));
+    }
+
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
